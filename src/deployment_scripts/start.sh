d3fdc557a65f7aa47efe80b51540cc4f32ad50f6
palakrishnateja <31216833+palakrishnateja@users.noreply.github.com>
2019-05-23 20:34:13 -0600

    included echo statements

--- src/deployment_scripts/start.sh
+++ src/deployment_scripts/start.sh
@@ -1,6 +1,9 @@
 #install all the requirements
 pip3 install -r ../../requirements.txt
+echo 'REQUIREMENTS ARE INSTALLED'
 #create a cron job to schedule the script
+echo 'CRON JOB CREATION'
 /usr/local/bin/python3 ../deployment_scripts/cron_job_runner.py
 #kickstart the http server
+echo 'HTTP SERVER CREATION AND LAUNCH'
 /usr/local/bin/python3 ../deployment_scripts/http_server_launcher.py
